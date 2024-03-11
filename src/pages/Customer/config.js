eee9306d8a3cd41729dd3cb8c5f4f952ad0f52d0
NAMTVPH23205 <127822190+NAMTVPH23205@users.noreply.github.com>
2024-03-11 21:34:35 +0700

    hehehehe

---
+++ src/pages/Customer/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage'
+const firebaseConfig = {
+    apiKey: "AIzaSyCRI84iA8h3T5R8Nh8BKNL6WNJ0w8pexeE",
+    authDomain: "imageuploaddb-efb38.firebaseapp.com",
+    projectId: "imageuploaddb-efb38",
+    storageBucket: "imageuploaddb-efb38.appspot.com",
+    messagingSenderId: "749154967680",
+    appId: "1:749154967680:web:05ef842a444fbb5076c665"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
+
+export { storage, firebaseApp };
+
+
